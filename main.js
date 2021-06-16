127def175bcdee59a1da7a22e9772439ac754f8b
CoderPulkit <75306428+CoderPulkit@users.noreply.github.com>
2021-06-16 13:25:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function one() {
+    var a=document.getElementById("p11").value;
+    var b=document.getElementById("p12").value;
+ var c=document.getElementById("p13").value; 
+   d=document.getElementById("p14").value;
+   var e=document.getElementById("p15").value;
+ var f=document.getElementById("p16").value;
+ var g=document.getElementById("p17").value;
+    var h=document.getElementById("p18").value;
+ var total=a+b+c+d+e+f+g+h;
+ document.getElementById("first")=total;
+}
+function two() {
+    var a=document.getElementById("p21").value;
+    var b=document.getElementById("p22").value;
+ var c=document.getElementById("p23").value; 
+   d=document.getElementById("p24").value;
+   var e=document.getElementById("p25").value;
+ var f=document.getElementById("p26").value;
+ var g=document.getElementById("p27").value;
+    var h=document.getElementById("p28").value;
+ var total=a+b+c+d+e+f+g;
+ document.getElementById("second")=total;
+}
